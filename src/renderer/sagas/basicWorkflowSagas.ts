24936dea192df8e561023fac080e7d623647464e
Agata Irene Ordano <agata@agata.localdomain>
2020-01-22 11:52:00 -0300

    testing asyncc commit
    default

--- src/renderer/sagas/basicWorkflowSagas.ts
+++ src/renderer/sagas/basicWorkflowSagas.ts
@@ -52,7 +52,7 @@ function* setCommitSuccessAlert() { // ! currently not working
     // -- Generator that yields a dispatch by the put() method as to update the changes area if
     // there's a change on the git status. 
 
-    yield delay(500)
+    yield delay(50)
 
     const successStatus = store.getState()?.basicWorkflowReducer.successStatus?._v.success
     const error = store.getState()?.basicWorkflowReducer.successStatus?._v.error 
