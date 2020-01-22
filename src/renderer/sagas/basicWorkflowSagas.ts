c422af3c9a9e29e6555de8b0974840b8603b152f
Agata Irene Ordano <agata@agata.localdomain>
2020-01-22 11:49:40 -0300

    testing async commit
    default

--- src/renderer/sagas/basicWorkflowSagas.ts
+++ src/renderer/sagas/basicWorkflowSagas.ts
@@ -51,7 +51,9 @@ function* clearCommitBox() { // ! currently not working
 function* setCommitSuccessAlert() { // ! currently not working
     // -- Generator that yields a dispatch by the put() method as to update the changes area if
     // there's a change on the git status. 
-    yield delay(500)
+
+    yield delay(3000)
+
     const successStatus = store.getState()?.basicWorkflowReducer.successStatus?._v.success
     const error = store.getState()?.basicWorkflowReducer.successStatus?._v.error 
 
